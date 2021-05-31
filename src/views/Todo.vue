3b44f56bf7427d4d01b33a4b1e3d64d2f9ed5e81
allen <eepson123@gmail.com>
2021-05-31 11:54:59 +0800

    add filter and button

---
+++ src/views/Todo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="todo">
+    <h1>todo</h1>
+    <p>show:{{ filter }}</p>
+    <router-link to="/todo" replace>All</router-link>
+    <router-link to="/todo?filter=active" replace>Active</router-link>
+    <router-link :to="{ path: '/todo', query: { filter: 'done' } }" replace
+      >Done</router-link
+    >
+    <div>{{ list }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      filter: 'all' // all,active,done
+    }
+  },
+  computed: {
+    list () {
+      return this.$store.getters.filterList(this.filter)
+    }
+  },
+  watch: {
+    // 資料轉變時可以使用，第一層不建議使用箭頭函式會把實體摧毀this
+    // $route: function (route) {
+    //   this.filter = route.query.filter || 'all'
+    // }
+    $route: {
+      immediate: true, // 只要進入component 馬上就執行
+      handler: function (route) {
+        // check query =>redirect
+        this.filter = route.query.filter || 'all'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.todo a {
+  color: black;
+  text-decoration: none;
+  display: inline-block;
+  width: 50px;
+  height: 30px;
+  background: grey;
+}
+.todo .router-link-exact-active {
+  color: green;
+}
+</style>
