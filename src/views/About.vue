a8c8efa87582149a69e3d0c9f22d6e685910adf7
allen <eepson123@gmail.com>
2021-06-03 13:38:27 +0800

    feat:add post list 新增資料連動區塊 issue#3

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,58 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an post number</h1>
+
+    <select v-model="chooseCity">
+      <option :value="null">請選擇</option>
+      <option v-for="(city, index) in citys" :key="index" :value="index">{{
+        city
+      }}</option>
+    </select>
+    <select v-model="distSelect">
+      <option :value="null">請選擇</option>
+      <option v-for="dist in districts" :key="dist.name" :value="dist.zip">{{
+        dist.name
+      }}</option>
+      <!-- <option
+        v-for="(dist, index) in districts"
+        :key="dist.zip"
+        :value="index"
+        >{{ dist.name }}</option
+      > -->
+    </select>
   </div>
 </template>
+<script>
+const postdata = require('@/assets/post.json')
+
+export default {
+  data () {
+    return {
+      chooseCity: null,
+      distSelect: null
+    }
+  },
+  computed: {
+    citys () {
+      return postdata.map(item => item.name)
+    },
+    districts () {
+      let districts = []
+      if (this.chooseCity !== null) {
+        districts = postdata[this.chooseCity].districts
+      }
+      return districts
+    }
+  },
+  watch: {
+    chooseCity: {
+      immediate: true,
+      handler () {
+        if (this.chooseCity !== null) {
+          this.distSelect = null
+        }
+      }
+    }
+  }
+}
+</script>
